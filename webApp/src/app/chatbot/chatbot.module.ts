3be6e739b9e960932d8784a13b6947276643fecc
Romain Estrada <62434967+Adartse-r@users.noreply.github.com>
2023-07-16 00:06:39 +0200

    Update

---
+++ webApp/src/app/chatbot/chatbot.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChatbotComponent } from './chatbot/chatbot.component';
+
+
+
+@NgModule({
+  declarations: [
+    ChatbotComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ChatbotModule { }
